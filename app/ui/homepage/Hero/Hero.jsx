c153cf1e40cd2674483e9bf9dd316a4dfc488dbf
moustafa ascoura <moustafaascoura@gmail.com>
2024-01-09 09:13:37 +0200

    updated services and programs section

--- app/ui/homepage/Hero/Hero.jsx
+++ app/ui/homepage/Hero/Hero.jsx
@@ -1,14 +1,53 @@
 import './Hero.css'
 import { Button } from '@mui/material'
 
-export default function Hero () {
-    return <>
+const stats = [
+  { name: 'Specialized Programs', value: '6+' },
+  { name: 'Skilled Coaches', value: '10+' },
+  { name: 'Calories Burnt', value: 'Unlimited' },
+]
+
+
+export default function Hero() {
+  return (
     <section className="hero" id="home">
-        <div>
-            <h1>READY TO TRAIN<span className='text-success block'>YOUR BODY</span></h1>
-            <p>Gym training is a structured and disciplined approach to physical exercise that focuses on strength, endurance and overall fitness improvement.</p>
+        {/* <div>
             <Button component="a" href="#contact" variant='outlined' color="success" className='text-3xl font-semibold px-20 py-6 rounded-0 mt-5'>Join Now</Button>
+        </div> */}
+
+      <div className="px-6 pt-6 sm:pt-6 lg:px-8">
+        <div className="mx-auto sm:max-w-2xl lg:max-w-5xl py-32 sm:py-24 lg:py-56">
+          <div className="text-center">
+            <h1 className="font-bold lg:text-8xl text-4xl">
+              READY TO TRAIN<span className='text-green-600 block'>YOUR BODY</span>
+              </h1>
+            <p className="mt-6 text-lg leading-6 sm:text-md">
+            Gym training is a structured and disciplined approach to physical 
+            exercise that focuses on strength, endurance and overall fitness improvement.
+            </p>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-4 sm:mt-20 sm:grid-cols-2 lg:grid-cols-3 text-center">
+            {stats.map((stat) => (
+              <div key={stat.name} className="flex flex-col-reverse">
+                <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
+                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+              </div>
+            ))}
+          </div>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#contact"
+                className="rounded-md bg-green-600 px-7 py-2 lg:py-4 text-md lg:text-2xl  font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              >
+                Get started
+              </a>
+              <a href="#services" className="text-sm font-semibold leading-6">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
         </div>
+      </div>
     </section>
-    </>
+  )
 }
--- app/ui/homepage/Programs/Programs.jsx
+++ app/ui/homepage/Programs/Programs.jsx
@@ -1,22 +1,23 @@
 import './Programs.css'
 
 export default function Programs () {
-    return <section id="programs" className="bg-dark text-center py-2 py-lg-5">
-        <div className='container'>
-            <h3 className='section-header my-3'><span className='border border-success line'></span>What We Do</h3>
-            <h1 className='fw-bold'>OUR <span className='text-success'>PROGRAMS</span></h1>
-            <div className="d-flex justify-content-between mt-5 mt-lg-5 slider">
-                <div>
+    return <section id="programs" className="bg-slate-950 text-center py-2 lg:py-5 ">
+        <div className='w-full max-w-7xl mx-auto my-16 px-8'>
+            <h3 className='section-header flex justify-center items-center text-2xl my-5'><span className='border-2 border-green-600 me-4 w-24'></span>What We Do</h3>
+            <h1 className='font-bold text-2xl lg:text-5xl'>OUR <span className='text-green-600'>PROGRAMS</span></h1>
+            
+            <div className="w-full flex flex-row overflow-x-scroll slider mt-8">
+                <div className='flex-shrink-0'>
                     <img src='/assets/images/Program1.jpg' alt="Martial Arts" />
-                    <h4 className='text-light w-100 caption'>Martial Arts Training</h4>
+                    <h4 className='w-full caption'>Martial Arts Training</h4>
                 </div>
-                <div className='mx-5'>
+                <div className='flex-shrink-0 mx-5'>
                     <img src='/assets/images/Program2.jpg' alt="" />
-                    <h4 className='text-light w-100 caption'>Rehabilitation</h4>
+                    <h4 className='w-full caption'>Rehabilitation</h4>
                 </div>
-                <div>
+                <div className='flex-shrink-0'>
                     <img src='/assets/images/Program3.jpg' alt="" />
-                    <h4 className='text-light w-100 caption'>Strength Excercises</h4>
+                    <h4 className='w-full caption'>Strength Excercises</h4>
                 </div>
             </div>
         </div>
